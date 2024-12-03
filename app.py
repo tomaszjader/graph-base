cec19badd579270c5f9e894a6e2942c573a3e0ea
Tomasz Jąder <47568523+tomaszjader@users.noreply.github.com>
2024-12-03 23:19:12 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,19 +2,16 @@ from neo4j import GraphDatabase
 import json
 
 
-NEO4J_URI = "bolt://localhost:7687"  # Adres Neo4j
-NEO4J_USER = "neo4j"  # Użytkownik (domyślnie "neo4j")
-NEO4J_PASSWORD = "password"  # Hasło
+NEO4J_URI = "bolt://localhost:7687"
+NEO4J_USER = "neo4j"
+NEO4J_PASSWORD = "password"
 
 
-# Funkcja do załadowania danych z JSON do Neo4j
+
 def load_data_to_neo4j(file_path):
-    # Ładowanie danych JSON
-    # Ładowanie danych JSON z wymuszonym kodowaniem UTF-8
     with open(file_path, "r", encoding="utf-8") as file:
         data = json.load(file)
 
-    # Połączenie z Neo4j
     driver = GraphDatabase.driver(NEO4J_URI, auth=(NEO4J_USER, NEO4J_PASSWORD))
 
     with driver.session() as session:
